e97c649dad5f40341facf76091f4b0e71b78c6f1
hyperminji <hyperminji@gmail.com>
2021-04-18 23:39:52 +0900

    20210418

---
+++ codeup_100/codeup6083.py
@@ -0,0 +1,9 @@
+# 6083 : [기초-종합] 빛 섞어 색 만들기(설명)(py)
+r,g,b = map(int, input().split())
+num=0
+for i in range(r):
+    for j in range(g):
+        for z in range(b):
+            num = num+1
+            print(i,j,z)
+print(num)            
