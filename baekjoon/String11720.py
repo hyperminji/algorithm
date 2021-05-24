68d766cc42803f93c7e57db14e6df705d0021929
hyperminji <hyperminji@gmail.com>
2021-05-24 23:43:47 +0900

    Update String11720.py

--- baekjoon/String11720.py
+++ baekjoon/String11720.py
@@ -1,1 +1,13 @@
 #https://www.acmicpc.net/problem/11720
+
+#input
+n=int(input())
+#두번째 숫자열
+numline = input()
+numlist = []
+
+for i in range(n):
+    numlist.append(int(numline[i]))
+    
+answer = sum(numlist)
+print(answer)
