8ec619dc8077f191c5727ab405652cf768bd2941
hyperminji <81575572+hyperminji@users.noreply.github.com>
2021-06-28 02:04:14 +0900

    Delete two-sum directory

--- two-sum/two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result_dic={}
-        for i, val in enumerate(nums):
-            if val in result_dic:
-                return [result_dic[val],i]
-            else:
-                result_dic[target-val] = i
-        
