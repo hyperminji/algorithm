4aff4c988f6346517b32dfa93c3d0990dda2bf73
hyperminji <81575572+hyperminji@users.noreply.github.com>
2021-10-14 10:23:02 +0900

    Create 2577.py

---
+++ baekjoon/2577.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/2577
+
+
+# input -> 연산 -> list, str처리하고 
+# count를 for에 넣고 돌리자 0~9
+# 첫째 줄에는 A × B × C의 결과에 0 이 몇 번 쓰였는지 출력한다. 
+# 마찬가지로 둘째 줄부터 열 번째 줄까지 A × B × C의 결과에 1부터 9까지의 숫자가 각각 몇 번 쓰였는지 차례로 한 줄에 하나씩 출력한다.
+a = int(input())
+b = int(input())
+c = int(input())
+result = a*b*c
+result = list(str(result))
+
+for i in range(10):
+    print(result.count(str(i)))
